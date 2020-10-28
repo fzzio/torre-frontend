e6dfb17190a72b6e17384e6a3c7cfa1d2464e521
Fabricio Orrala <fabricio_fdop_007@hotmail.com>
2020-10-28 00:11:15 -0500

    Candidate

--- src/components/CandidateList.vue
+++ src/components/CandidateList.vue
@@ -5,7 +5,7 @@
           <div class="column is-6" >
             <span class="is-size-5-desktop is-size-6-mobile has-text-grey" v-if="pageType !== 'bookmarks'"> Search Results </span>
             <span class="is-size-5-desktop is-size-6-mobile has-text-grey" v-else> Bookmarks</span></div>
-          <div class="column is-5 has-text-right "><span class="has-text-grey-light is-size-6"> {{candidates.length}} album(s) </span> </div>
+          <div class="column is-5 has-text-right "><span class="has-text-grey-light is-size-6"> {{candidates.length}} candidate(s) </span> </div>
           <div class="column is-1 has-text-left">
               <b-tooltip type="is-light" label="switch panel view" position="is-top" :active="!isMobile">
               <i @click="onClickUpdateSettings" class="fas  fa-lg" :class="[settings.panelType === 'card' ? 'fa-th-list' : 'fa-th']"></i>
@@ -17,39 +17,39 @@
           <div class="columns is-multiline is-mobile" v-if="!isCandidateLoading && displayedCandidates.length > 0" :key="pageType">
               <div  class="column"
                 :class="[settings.panelType === 'card' ? 'is-3-widescreen is-3-desktop is-4-tablet' : 'is-4-widescreen  is-4-desktop is-6-tablet is-12-mobile']"
-                v-for="album in displayedCandidates"
-                :key="album.collectionId">
+                v-for="candidate in displayedCandidates"
+                :key="candidate.collectionId">
                 <!-- Card Panel  -->
                 <div class="card"
                   v-if="settings.panelType === 'card'" >
                   <div class="card-image">
                     <figure class="image is-4by3">
                       <img
-                        :src="replaceArtworkUrlSize(album.artworkUrl100, '300x250')"
-                        :alt="album.collectionCensoredName">
+                        :src="replaceArtworkUrlSize(candidate.artworkUrl100, '300x250')"
+                        :alt="candidate.collectionCensoredName">
                     </figure>
                   </div>
                   <div class="card-content">
                     <div class="media">
                       <div class="media-content overflow-content">
-                        <div class="title is-size-6-widescreen is-size-6-desktop candidate-name" ><a v-if="album.collectionId" @click="onClickAlbumName(album.collectionId)">{{album.collectionCensoredName}}</a></div>
-                        <div class="subtitle is-6">{{album.artistName}} <br>
-                        <span class="has-text-grey-light">{{album.primaryGenreName}}</span></div>
+                        <div class="title is-size-6-widescreen is-size-6-desktop candidate-name" ><a v-if="candidate.collectionId" @click="onClickAlbumName(candidate.collectionId)">{{candidate.collectionCensoredName}}</a></div>
+                        <div class="subtitle is-6">{{candidate.artistName}} <br>
+                        <span class="has-text-grey-light">{{candidate.primaryGenreName}}</span></div>
                       </div>
                     </div>
                   </div>
                   <footer class="card-footer">
-                      <a :href="album.collectionViewUrl" target="_blank" class="card-footer-item">
+                      <a :href="candidate.collectionViewUrl" target="_blank" class="card-footer-item">
                         <b-tooltip type="is-light" label="Download on iTunes" position="is-top" :active="!isMobile">
                           <i class="fab fa-itunes-note"></i>
                         </b-tooltip>
                       </a>
                       <span class="heart card-footer-item">
-                        <b-tooltip type="is-light" :label="isInBookmark(album.collectionCensoredName) ? 'click to unbookmarked' : 'click to bookmark'" position="is-top" :active="!isMobile">
-                          <i @click="clickBookmarkCandidate(album)" class="fas fa-lg bookmarkIcon" :class="[{'favorite': isInBookmark(album.collectionCensoredName)}, settings.bookmarkIcon]"></i>
+                        <b-tooltip type="is-light" :label="isInBookmark(candidate.collectionCensoredName) ? 'click to unbookmarked' : 'click to bookmark'" position="is-top" :active="!isMobile">
+                          <i @click="clickBookmarkCandidate(candidate)" class="fas fa-lg bookmarkIcon" :class="[{'favorite': isInBookmark(candidate.collectionCensoredName)}, settings.bookmarkIcon]"></i>
                         </b-tooltip>
                       </span>
-                      <a v-if="settings.youtubeLink === 'true'" :href="`https://www.youtube.com/results?search_query=${album.artistName} - ${album.collectionCensoredName}`" target="_blank" class="card-footer-item">
+                      <a v-if="settings.youtubeLink === 'true'" :href="`https://www.youtube.com/results?search_query=${candidate.artistName} - ${candidate.collectionCensoredName}`" target="_blank" class="card-footer-item">
                         <b-tooltip type="is-light" label="search on youtube" position="is-top" :active="!isMobile">
                           <i class="fab fa-youtube"></i>
                         </b-tooltip>
@@ -60,29 +60,29 @@
                 <article class="media media-wrap" v-if="settings.panelType === 'media'">
                   <figure class="media-left">
                     <p class="image ">
-                      <img :src="replaceArtworkUrlSize(album.artworkUrl100, '130x130')" :alt="album.collectionCensoredName">
+                      <img :src="replaceArtworkUrlSize(candidate.artworkUrl100, '130x130')" :alt="candidate.collectionCensoredName">
                     </p>
                   </figure>
                   <div class="media-content">
                     <div class="content overflow-content">
                       <div>
-                        <strong><a v-if="album.collectionId" @click="onClickAlbumName(album.collectionId)">{{album.collectionCensoredName}}</a></strong> <br>
-                        {{album.artistName}} ( <span class="has-text-grey-light">{{album.primaryGenreName}}</span> )
+                        <strong><a v-if="candidate.collectionId" @click="onClickAlbumName(candidate.collectionId)">{{candidate.collectionCensoredName}}</a></strong> <br>
+                        {{candidate.artistName}} ( <span class="has-text-grey-light">{{candidate.primaryGenreName}}</span> )
                       </div>
                     </div>
                     <div class="level is-mobile">
                       <div class="level-left">
-                        <a  class="level-item" :href="album.collectionViewUrl" target="_blank">
+                        <a  class="level-item" :href="candidate.collectionViewUrl" target="_blank">
                           <b-tooltip type="is-light" label="Download on iTunes" position="is-top" :active="!isMobile">
                             <i class="fab fa-itunes-note"></i>
                           </b-tooltip>
                         </a>
                         <a class="level-item">
-                          <b-tooltip type="is-light" :label="isInBookmark(album.collectionCensoredName) ? 'click to unbookmarked' : 'click to bookmark'" position="is-top" :active="!isMobile">
-                            <i @click="clickBookmarkCandidate(album)" class="fas bookmarkIcon" :class="[{'favorite': isInBookmark(album.collectionCensoredName)}, settings.bookmarkIcon]"></i>
+                          <b-tooltip type="is-light" :label="isInBookmark(candidate.collectionCensoredName) ? 'click to unbookmarked' : 'click to bookmark'" position="is-top" :active="!isMobile">
+                            <i @click="clickBookmarkCandidate(candidate)" class="fas bookmarkIcon" :class="[{'favorite': isInBookmark(candidate.collectionCensoredName)}, settings.bookmarkIcon]"></i>
                           </b-tooltip>
                         </a>
-                        <a v-if="settings.youtubeLink === 'true'" class="level-item" :href="`https://www.youtube.com/results?search_query=${album.artistName} - ${album.collectionCensoredName}`" target="_blank">
+                        <a v-if="settings.youtubeLink === 'true'" class="level-item" :href="`https://www.youtube.com/results?search_query=${candidate.artistName} - ${candidate.collectionCensoredName}`" target="_blank">
                           <b-tooltip type="is-light" label="search on youtube" position="is-top" :active="!isMobile">
                             <i class="fab fa-youtube"></i>
                           </b-tooltip>
