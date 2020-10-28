70d4580b402753d3b9ec6193979fd93464b6bfd7
Fabricio Orrala <fabricio_fdop_007@hotmail.com>
2020-10-28 15:04:08 -0500

    heroku integration

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api/': {
-        // target: 'http://localhost:3007',
-        target: 'http://ec2-3-15-140-212.us-east-2.compute.amazonaws.com:3007',
+        target: 'https://floating-everglades-26974.herokuapp.com/',
         changeOrigin: true,
         pathRewrite: { '^/api/': '' }
       }
