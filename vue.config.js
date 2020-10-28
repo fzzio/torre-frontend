7a6646f098f3b2878101459e32b50540ef4e1ecf
Fabricio Orrala <fabricio_fdop_007@hotmail.com>
2020-10-28 07:59:14 -0500

    hotfix fix url instance

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/api/': {
-        // target: 'https://itunes.apple.com',
-        target: 'http://localhost:3007',
+        // target: 'http://localhost:3007',
+        target: 'http://ec2-3-15-140-212.us-east-2.compute.amazonaws.com:3007',
         changeOrigin: true,
         pathRewrite: { '^/api/': '' }
       }
